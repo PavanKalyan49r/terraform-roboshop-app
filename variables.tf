81d5a4c1a9002e6d90020c7423902e4f9e1b6b99
PavanKalyan49r <pavan49r@gmail.com>
2024-04-27 10:35:17 +0530

    roboshop

---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "common_tags" {
+#   default = {
+#     project = "roboshop"
+#     environment = "dev"
+#     terraform = "true"
+#   }
+}
+
+variable "tags" {
+  
+}
+
+variable "project_name" {
+    # default = "roboshop" 
+}
+
+variable "environment" {
+#   default = "dev"
+}
+
+variable "zone_name" {
+#   default = "pavanaws.online"
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "Component_sg_id" {
+  
+}
+
+variable "private_subnet_ids" {
+  
+}
+
+variable "iam_instance_profile" {
+  
+}
+
+variable "app_alb_listener_arn" {
+  
+}
+
+variable "rule_priority" {
+  
+}
+
+variable "app_version" {
+  
+}
